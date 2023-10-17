5d2ffc1043e879e5ea00197b15dcf40e12842c32
Norik <noriksaroyan@gmail.com>
2023-10-18 00:22:10 +0300

    Init

---
+++ app/src/main/java/ru/team1802/mypostmail/entities/Post.java
@@ -0,0 +1,4 @@
+package ru.team1802.mypostmail.entities;
+
+public class Post {
+}
---
+++ app/src/main/java/ru/team1802/mypostmail/entities/User.java
@@ -0,0 +1,41 @@
+package ru.team1802.mypostmail.entities;
+
+public class User {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String email;
+
+}
