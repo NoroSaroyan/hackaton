9581cd88aa5d09cfdecffbc22f7b6662eaa96ed5
Daniel <daniel.verdanov@yandex.ru>
2023-10-18 02:18:11 +0300

    text

--- app/src/main/java/ru/team1802/mypostmail/LoginActivity.java
+++ app/src/main/java/ru/team1802/mypostmail/LoginActivity.java
@@ -1,15 +1,16 @@
 package ru.team1802.mypostmail;
 
+
+import androidx.appcompat.R;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
 
 }
