634f1a6f6c38c69e85a594ac338fc933c4303bb3
Daniel <daniel.verdanov@yandex.ru>
2023-10-18 03:54:09 +0300

    Созданы первый activity, считывание данных с текстовых полей

--- app/src/main/java/ru/team1802/mypostmail/LoginActivity.java
+++ app/src/main/java/ru/team1802/mypostmail/LoginActivity.java
@@ -2,13 +2,43 @@ package ru.team1802.mypostmail;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Создание объектов интерфейса
+    EditText loginField;
+    EditText passwordField;
+    TextView forgotPassword;
+    Button buttonLogin;
+
+    String login;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Поиск обектов интерфейса по их id
+        loginField = findViewById(R.id.login_field);
+        passwordField = findViewById(R.id.password_field);
+        forgotPassword = findViewById(R.id.forgot_password);
+        buttonLogin = findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Считывание данных с полей ввода и запись в переменные
+                login = loginField.getText().toString();
+                password = passwordField.getText().toString();
+
+                //Код который выполняется при нажатии на кнопку "Войти"
+            }
+        });
     }
 
 }
